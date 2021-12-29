d20f74eba402eced3d289255304a0649965626db
wn <chrissignore@gmail.com>
2021-12-29 16:20:07 -0500

    v1.0

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,18 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Mirin</title>
-        <meta name="description" content="mirin.xyz" />
+        <title>OpenBoard</title>
+        <meta name="description" content="openboard.xyz" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-       <h1>Launching 2022</h1>
+       <h1>OpenBoard</h1>
       </main>
     </div>
   )
